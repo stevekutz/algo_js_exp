edb2db129445f08a5d986a1f2376d59005daa61a
stevekutz <stkutz@gmail.com>
2019-12-02 23:03:49 -0600

    setup basic formatting with Semantic UI React for table layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Dropdown, Form, Input, Label, Modal, Segment, Table} from 'semantic-ui-react';
+
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Segment>
+        <Label> Algorithms </Label>
+        <Form>
+            <Label> Enter length of random array </Label>
+            <Input></Input>
+            <Segment>
+                <Button> Create random numbers </Button>
+            </Segment>
+        </Form>
+     
+        <Segment style = {{width: '50%', margin: '0 auto'}}>
+        <Table>
+            <Table.Body>
+                <Table.Row>
+                    {testArr.map((item, index) => (
+
+                        <Table.Cell 
+                            style = {{border: '1px solid pink'}}
+                            // collapsing 
+                            // width = {1}
+                            textAlign = 'center'
+                        >  
+                        {item}</Table.Cell>
+                        ))}
+                </Table.Row>
+            </Table.Body>
+        </Table>
+
+        </Segment>    
+
+    </Segment>
   );
 }
 
