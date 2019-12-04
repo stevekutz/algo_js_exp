ca0029898db41dc14d56605b9640eb0974130280
stevekutz <stkutz@gmail.com>
2019-12-03 21:24:53 -0600

    created table of random integers

--- app/src/App.js
+++ app/src/App.js
@@ -1,43 +1,61 @@
 import React from 'react';
 import {Button, Dropdown, Form, Input, Label, Modal, Segment, Table} from 'semantic-ui-react';
+import {useState} from 'reinspect';
 
-const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 
-function App() {
-  return (
-    <Segment>
-        <Label> Algorithms </Label>
-        <Form>
-            <Label> Enter length of random array </Label>
-            <Input></Input>
+const App = () => {
+
+
+    const [testArrLength, settestArrLength] = useState( 10, 'TestArrayLength');
+    const [testArr, setTestArr] = useState([], 'Test Array');
+
+    const makeRandArr = () => {
+        const arr = [];
+        
+        for(let i = 0; i < testArrLength; i++){
+            arr.push(Math.floor((Math.random() * testArrLength) + 1))
+        }
+        console.log(arr);
+        setTestArr(arr);
+    }
+
+    return (
+        <Segment>
+            <Label> Algorithms </Label>
             <Segment>
-                <Button> Create random numbers </Button>
+                <Label> Enter length of random array </Label>
+                <Input></Input>
+                <Segment>
+                    <Button onClick = {makeRandArr}> Create random numbers </Button>
+                </Segment>
             </Segment>
-        </Form>
-     
-        <Segment style = {{width: '50%', margin: '0 auto'}}>
-        <Table>
-            <Table.Body>
-                <Table.Row>
-                    {testArr.map((item, index) => (
+            
+        
+            <Segment style = {{width: '50%', margin: '0 auto'}}>
+            <Table>
+                <Table.Body>
+                    <Table.Row>
+                        {testArr.map((item, index) => (
 
-                        <Table.Cell 
-                            style = {{border: '1px solid pink'}}
-                            // collapsing 
-                            // width = {1}
-                            textAlign = 'center'
-                        >  
-                        {item}</Table.Cell>
-                        ))}
-                </Table.Row>
-            </Table.Body>
-        </Table>
+                            <Table.Cell 
+                                key = {index}
+                                style = {{border: '1px solid pink'}}
+                                // collapsing 
+                                // width = {1}
+                                textAlign = 'center'
+                            >  
+                            {item}</Table.Cell>
+                            ))}
+                    </Table.Row>
+                </Table.Body>
+            </Table>
 
-        </Segment>    
+            </Segment>    
 
-    </Segment>
-  );
+        </Segment>
+    );
 }
 
 export default App;
