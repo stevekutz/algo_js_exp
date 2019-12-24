990163cb41668d2017b67e9363befb8feb1055b3
stevekutz <stkutz@gmail.com>
2019-12-24 15:31:43 -0600

    updated serialize-javascript

--- app/src/comp/BarChart.js
+++ app/src/comp/BarChart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Chart, Dataset} from 'react-rainbow-components';
 
-
 const BarChart = ({title, labels, values}) => {
 
     return (
@@ -13,7 +12,10 @@ const BarChart = ({title, labels, values}) => {
 
 }
 
+
 export default BarChart;
 
 // <Dataset title = {title} values = {values} backgroundColor = 'lightblue'/>
 // <Dataset title = 'active' values = {[undefined, 1.5]} backgroundColor = 'pink'/>
+
+
